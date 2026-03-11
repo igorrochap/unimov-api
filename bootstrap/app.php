18a502d4d1b0d6c0da167878000505abf3492d91
Eduardo Pereira <doardoperera@gmail.com>
2026-03-11 17:24:56 -0300

    resolve merge conflit: Adicionado sanctum middleware nas rotas de períodos

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -11,12 +12,14 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function () {
-            Route::middleware(['api'])->prefix('api/usuarios')->group(base_path('routes/api/usuarios.php'));
-            Route::middleware(['api'])->prefix('api/periodos')->group(base_path('routes/api/periodos.php'));
+            Route::middleware(['api', 'auth:sanctum', 'perfil:admin'])->prefix('api/usuarios')->group(base_path('routes/api/usuarios.php'));
+            Route::middleware(['api', 'auth:sanctum'])->prefix('api/periodos')->group(base_path('routes/api/periodos.php'));
         }
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->prepend(HandleCors::class);
+        $middleware->statefulApi();
+        $middleware->alias(['perfil' => \App\Http\Middleware\VerificaPerfil::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
